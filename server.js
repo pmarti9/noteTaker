9071499acc5aa1773c1bc46ca80ed46b44292163
pmarti99 <parkerjmartin883@gmail.com>
2020-05-16 18:34:26 -0600

    moved server.js outside of develop folder

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.static('public'));
 
 
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./Develop/routes/apiRoutes")(app);
+require("./Develop/routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
